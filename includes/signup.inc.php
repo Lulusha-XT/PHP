8112d8a4f691c8a59418428b57c1925ae4ef220d
Lulusha-XT <leulsileshi7@gmail.com>
2022-08-14 20:55:52 +0300

    prepared statment

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -11,6 +11,71 @@ if (isset($_POST['signup-submit'])) {
 
     if (empty($userName) || empty($email) || empty($password) || empty($passwordRepeat)) {
         header("Location: ../signup.php?error=emptyfileds&uid=".$userName."&mail=".$email);
+        exit();
+    }
+    elseif (!filter_var($email,FILTER_VALIDATE_EMAIL) && !preg_match("/^[a-zA-Z0-9]*$/",$userName)) {
+        header("Location: ../signup.php?error=invalidmail&uid");
+        exit();
     }
 
+    else if (!filter_var($email,FILTER_VALIDATE_EMAIL)) {
+            header("Location: ../signup.php?error=invalidmail&uid=".$userName);
+            exit();
+    }
+
+    else if (!preg_match("/^[a-zA-Z0-9]*$/",$userName)) {
+        header("Location: ../signup.php?error=invaliduid&mail=".$email);
+        exit();
+    }
+
+    elseif ($password != $passwordRepeat) {
+        header("Location: ../signup.php?error=passwordchek&mail=".$email ."uid=".$userName);
+        exit();
+    }
+
+    else {
+        $sql = "SELECT uidUser FROM users WHERE uidusers=?";
+        $stmt = mysqli_stmt_init($conn);
+        if (!mysqli_stmt_prepare($stmt, $sql)) {
+            header("Location: ../signup.php?error=sqlerror");
+            exit();
+        }
+        else {
+
+            mysqli_stmt_bind_param($stmt, "s", $userName);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_store_result($stmt);
+            $resultCheck = mysqli_stmt_num_rows($stmt);
+            if ($resultCheck > 0) {
+                header("Location: ../signup.php?error=usernametaken&mail=".$email);
+                exit();
+            }
+            else {
+                $sql = "INSERT INTO users(uidUsers, emailUsers, pwdUsers) VALUES (?,?,?)";
+                $stmt = mysqli_stmt_init($conn);
+                if (!mysqli_stmt_prepare($stmt, $sql)) {
+                    header("Location: ../signup.php?error=sqlerror");
+                    exit();
+            }
+            else {
+                $hashedpwd = password_hash($password, PASSWORD_DEFAULT);
+
+                mysqli_stmt_bind_param($stmt, "sss", $userName, $email, $password);
+                mysqli_stmt_execute($stmt);
+                header("Location: ../signup.php?signup=success");
+                    exit();
+
+            }
+
+        }
+    }
+
+}
+mysqli_stmt_close($stmt);
+mysqli_close($conn);
+
+}
+else {
+    header("Location: ../signup.php");
+    exit();
 }
